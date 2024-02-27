68be9f1f798fdeeb3ebc05ebbb6dade4bcc32b31
Michael Tan <60373935+MrMikol@users.noreply.github.com>
2024-02-27 23:51:58 +0000

    printversion function

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 import datetime
 
-response = requests.get("https://github.com/paritytech/polkadot-sdk/releases/latest")
-polkadot = response.url.split("/").pop()
+#print(polkadot)
 
-print(polkadot)
+#f = open('output.txt', 'a')
+#f.write(polkadot)
 
-f = open('output.txt', 'a')
-f.write(polkadot)
+def printVersions():
+    response = requests.get("https://github.com/paritytech/polkadot-sdk/releases/latest")
+    polkadot = response.url.split("/").pop()
+
+    f = open('output.txt', 'w')
+    f.write("The latest version of Polkadot is " + polkadot)
+
+printVersions()
