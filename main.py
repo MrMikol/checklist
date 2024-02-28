fdeec94f96894b224a90b6ffdf17f3e12e6ce36a
Michael Tan <60373935+MrMikol@users.noreply.github.com>
2024-02-28 00:37:44 +0000

    array and function added

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ import datetime
 #f = open('output.txt', 'a')
 #f.write(polkadot)
 
-def printVersions():
-    response = requests.get("https://github.com/paritytech/polkadot-sdk/releases/latest")
+github = ["https://github.com/paritytech/polkadot-sdk/releases/latest", "https://github.com/prysmaticlabs/prysm/releases/latest", "https://github.com/rocket-pool/smartnode-install/releases/latest", "https://github.com/ethereum/go-ethereum/releases/latest"]
+date = datetime.datetime.now()
+
+def printVersions(github, date):
+    f.write(str(date.strftime("%x")) + '\n')
+    response = requests.get(github[0])
     polkadot = response.url.split("/").pop()
 
     f = open('output.txt', 'w')
     f.write("The latest version of Polkadot is " + polkadot)
 
-printVersions()
+printVersions(github, date)
